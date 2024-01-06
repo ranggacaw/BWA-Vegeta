1c4be0d739ab54a5294502dc299b7707b407ab85
unknown <ranggacahyaw@gmail.com>
2024-01-06 18:06:41 +0700

    api intro

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    return NextResponse.json({
+        success: true,
+        message: 'Get all users!',
+        data: [
+            {
+                id: 1,
+                name: "Taufan"
+            },
+            {
+                id: 2,
+                name: "Duwi"
+            },
+        ]
+    },
+    {
+        // status 200 di pakai ketika berhasil, kalo misal ga ada biasana pake 404
+        status: 200,
+    })
+}
+
+export async function POST() {
+    return NextResponse.json({
+        success: true,
+        message: 'New user created!',
+        data: [
+            {
+                id: 3,
+                name: "Ipul"
+            },
+        ]
+    },
+    {
+        // status 200 di pakai ketika berhasil, kalo misal ga ada biasana pake 404
+        status: 200,
+    })
+}
